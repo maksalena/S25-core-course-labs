21dbf20d1fce5817ae6a6b90de732655e944ca30
Alyona <maksalena04@mail.ru>
2025-03-09 23:32:02 +0300

    Update app.py

--- app_python/app.py
+++ app_python/app.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 import pytz
 from flask import Flask
-from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 # Create a Flask application instance
